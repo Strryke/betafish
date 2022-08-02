c1d132921a480da4312f599a61b360065a75102d
Strryke <gavinong2000@gmail.com>
2022-08-02 16:18:12 +0800

    added position table + first negamax algorithm

---
+++ pages/evaluation.js
@@ -0,0 +1,159 @@
+var weights = { p: 100, n: 280, b: 320, r: 479, q: 929, k: 60000, k_e: 60000 };
+var pst_w = {
+  p: [
+    [100, 100, 100, 100, 105, 100, 100, 100],
+    [78, 83, 86, 73, 102, 82, 85, 90],
+    [7, 29, 21, 44, 40, 31, 44, 7],
+    [-17, 16, -2, 15, 14, 0, 15, -13],
+    [-26, 3, 10, 9, 6, 1, 0, -23],
+    [-22, 9, 5, -11, -10, -2, 3, -19],
+    [-31, 8, -7, -37, -36, -14, 3, -31],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ],
+  n: [
+    [-66, -53, -75, -75, -10, -55, -58, -70],
+    [-3, -6, 100, -36, 4, 62, -4, -14],
+    [10, 67, 1, 74, 73, 27, 62, -2],
+    [24, 24, 45, 37, 33, 41, 25, 17],
+    [-1, 5, 31, 21, 22, 35, 2, 0],
+    [-18, 10, 13, 22, 18, 15, 11, -14],
+    [-23, -15, 2, 0, 2, 0, -23, -20],
+    [-74, -23, -26, -24, -19, -35, -22, -69],
+  ],
+  b: [
+    [-59, -78, -82, -76, -23, -107, -37, -50],
+    [-11, 20, 35, -42, -39, 31, 2, -22],
+    [-9, 39, -32, 41, 52, -10, 28, -14],
+    [25, 17, 20, 34, 26, 25, 15, 10],
+    [13, 10, 17, 23, 17, 16, 0, 7],
+    [14, 25, 24, 15, 8, 25, 20, 15],
+    [19, 20, 11, 6, 7, 6, 20, 16],
+    [-7, 2, -15, -12, -14, -15, -10, -10],
+  ],
+  r: [
+    [35, 29, 33, 4, 37, 33, 56, 50],
+    [55, 29, 56, 67, 55, 62, 34, 60],
+    [19, 35, 28, 33, 45, 27, 25, 15],
+    [0, 5, 16, 13, 18, -4, -9, -6],
+    [-28, -35, -16, -21, -13, -29, -46, -30],
+    [-42, -28, -42, -25, -25, -35, -26, -46],
+    [-53, -38, -31, -26, -29, -43, -44, -53],
+    [-30, -24, -18, 5, -2, -18, -31, -32],
+  ],
+  q: [
+    [6, 1, -8, -104, 69, 24, 88, 26],
+    [14, 32, 60, -10, 20, 76, 57, 24],
+    [-2, 43, 32, 60, 72, 63, 43, 2],
+    [1, -16, 22, 17, 25, 20, -13, -6],
+    [-14, -15, -2, -5, -1, -10, -20, -22],
+    [-30, -6, -13, -11, -16, -11, -16, -27],
+    [-36, -18, 0, -19, -15, -15, -21, -38],
+    [-39, -30, -31, -13, -31, -36, -34, -42],
+  ],
+  k: [
+    [4, 54, 47, -99, -99, 60, 83, -62],
+    [-32, 10, 55, 56, 56, 55, 10, 3],
+    [-62, 12, -57, 44, -67, 28, 37, -31],
+    [-55, 50, 11, -4, -19, 13, 0, -49],
+    [-55, -43, -52, -28, -51, -47, -8, -50],
+    [-47, -42, -43, -79, -64, -32, -29, -32],
+    [-4, 3, -14, -50, -57, -18, 13, 4],
+    [17, 30, -3, -14, 6, -1, 40, 18],
+  ],
+
+  // Endgame King Table
+  k_e: [
+    [-50, -40, -30, -20, -20, -30, -40, -50],
+    [-30, -20, -10, 0, 0, -10, -20, -30],
+    [-30, -10, 20, 30, 30, 20, -10, -30],
+    [-30, -10, 30, 40, 40, 30, -10, -30],
+    [-30, -10, 30, 40, 40, 30, -10, -30],
+    [-30, -10, 20, 30, 30, 20, -10, -30],
+    [-30, -30, 0, 0, 0, 0, -30, -30],
+    [-50, -30, -30, -30, -30, -30, -30, -50],
+  ],
+};
+var pst_b = {
+  p: pst_w["p"].slice().reverse(),
+  n: pst_w["n"].slice().reverse(),
+  b: pst_w["b"].slice().reverse(),
+  r: pst_w["r"].slice().reverse(),
+  q: pst_w["q"].slice().reverse(),
+  k: pst_w["k"].slice().reverse(),
+  k_e: pst_w["k_e"].slice().reverse(),
+};
+
+var pstOpponent = { w: pst_b, b: pst_w };
+var pstSelf = { w: pst_w, b: pst_b };
+
+/*
+ * Evaluates the board at this point in time,
+ * using the material weights and piece square tables.
+ */
+export function evaluateBoard(move, prevSum, color) {
+  var from = [
+    8 - parseInt(move.from[1]),
+    move.from.charCodeAt(0) - "a".charCodeAt(0),
+  ];
+  var to = [
+    8 - parseInt(move.to[1]),
+    move.to.charCodeAt(0) - "a".charCodeAt(0),
+  ];
+
+  // Change endgame behavior for kings
+  if (prevSum < -1500) {
+    if (move.piece === "k") {
+      move.piece = "k_e";
+    } else if (move.captured === "k") {
+      move.captured = "k_e";
+    }
+  }
+
+  if ("captured" in move) {
+    // Opponent piece was captured (good for us)
+    if (move.color === color) {
+      prevSum +=
+        weights[move.captured] +
+        pstOpponent[move.color][move.captured][to[0]][to[1]];
+    }
+    // Our piece was captured (bad for us)
+    else {
+      prevSum -=
+        weights[move.captured] +
+        pstSelf[move.color][move.captured][to[0]][to[1]];
+    }
+  }
+
+  if (move.flags.includes("p")) {
+    // NOTE: promote to queen for simplicity
+    move.promotion = "q";
+
+    // Our piece was promoted (good for us)
+    if (move.color === color) {
+      prevSum -=
+        weights[move.piece] + pstSelf[move.color][move.piece][from[0]][from[1]];
+      prevSum +=
+        weights[move.promotion] +
+        pstSelf[move.color][move.promotion][to[0]][to[1]];
+    }
+    // Opponent piece was promoted (bad for us)
+    else {
+      prevSum +=
+        weights[move.piece] + pstSelf[move.color][move.piece][from[0]][from[1]];
+      prevSum -=
+        weights[move.promotion] +
+        pstSelf[move.color][move.promotion][to[0]][to[1]];
+    }
+  } else {
+    // The moved piece still exists on the updated board, so we only need to update the position value
+    if (move.color !== color) {
+      prevSum += pstSelf[move.color][move.piece][from[0]][from[1]];
+      prevSum -= pstSelf[move.color][move.piece][to[0]][to[1]];
+    } else {
+      prevSum -= pstSelf[move.color][move.piece][from[0]][from[1]];
+      prevSum += pstSelf[move.color][move.piece][to[0]][to[1]];
+    }
+  }
+
+  return prevSum;
+}
--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,26 @@ const Chessboard = dynamic(() => import("chessboardjsx"), {
   ssr: false, // <- this do the magic ;)
 });
 
+var reverse_array = function (array) {
+  return array.slice().reverse();
+};
+
 export default function Home() {
   const [position, setPosition] = useState("start");
   const [game, setGame] = useState();
+  const [gameState, setGameState] = useState("");
+
+  var pawn_value = 100;
+  var knight_value = 320;
+  var bishop_value = 330;
+  var rook_value = 500;
+  var queen_value = 900;
+  var king_value = 20000;
 
   useEffect(() => {
     setGame(new Chess());
     // setTimeout(() => randomMove(), 1000);
+    setGameState("White to move");
   }, []);
 
   const onDrop = ({ sourceSquare, targetSquare }) => {
@@ -26,103 +39,298 @@ export default function Home() {
       promotion: "q", // NOTE: always promote to a queen for example simplicity
     });
 
-    setPosition(game.fen());
-
     // illegal move
     if (move === null) return "snapback";
-
+    setPosition(game.fen());
+    setGameState("Black to move");
+    if (game.in_checkmate()) {
+      setGameState("White wins");
+      console.log(game.pgn());
+    }
     // make random legal move for black
-    window.setTimeout(randomMove, 300);
-  };
-
-  const boardScore = (fen) => {
-    // fen = rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
-    // caps are for white
-    // white is maximizing player
-    const pieceWorth = {
-      p: -1,
-      P: 1,
-      k: -3,
-      K: 3,
-      b: -3,
-      B: 3,
-      r: -5,
-      R: 5,
-      q: -3,
-      Q: 3,
-      k: -1000,
-      K: 1000,
-    };
-    const pieces = fen.split(" ")[0].split("");
-    const score = 0;
-    for (const piece in pieces) {
-      score += pieceWorth[pieces[piece]] || 0;
+    window.setTimeout(() => makeMove(), 300);
+    if (game.in_checkmate()) {
+      setGameState("Black wins");
+      console.log(game.pgn());
     }
-    return score;
+    setGameState("White to move");
   };
 
-  //https://www.chessprogramming.org/Negamax
-  const rootNegamax = (game, depth, isMaximizing) => {
-    const possibleMoves = game.moves();
-    const bestMove = null;
-    const bestScore = -9999;
-    for (const move of possibleMoves) {
-      game.move(move);
-      const value = negamax(game, depth - 1, !isMaximizing);
-      game.undo();
-      if (value > bestScore) {
-        bestScore = value;
-        bestMove = move;
+  const makeMove = () => {
+    game.move(rootnegamax());
+    setPosition(game.fen());
+  };
+
+  // const boardScore = (fen) => {
+  //   // fen = rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+  //   // caps are for white
+  //   // white is maximizing player
+  //   const pieceWorth = {
+  //     p: -1,
+  //     P: 1,
+  //     k: -3,
+  //     K: 3,
+  //     b: -3,
+  //     B: 3,
+  //     r: -5,
+  //     R: 5,
+  //     q: -3,
+  //     Q: 3,
+  //     k: -99999,
+  //     K: 99999,
+  //   };
+  //   const pieces = fen.split(" ")[0].split("");
+  //   const score = 0;
+  //   for (const piece in pieces) {
+  //     score += pieceWorth[pieces[piece]] || 0;
+  //   }
+
+  //   if (game.turn() === "b" && game.in_checkmate()) score += 99999999;
+  //   if (game.turn() === "w" && game.in_checkmate()) score -= 99999999;
+
+  //   return score;
+  // };
+
+  var evaluate = function () {
+    if (game.in_checkmate()) {
+      return game.turn() === "w" ? 999999 : -999999;
+    }
+    if (
+      game.in_threefold_repetition() ||
+      game.in_stalemate() ||
+      game.in_draw()
+    ) {
+      return game.turn() === "w" ? 999999 : -999999;
+    }
+
+    var total = 0;
+
+    // loop through the board
+    for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 8; j++) {
+        // get every piece
+        var piece = game.get(String.fromCharCode(97 + i) + (j + 1));
+
+        // exit as soon as possible if there's not piece
+        if (!piece) {
+          continue;
+        }
+
+        /* add the piece value
+         * p > pawn > 100
+         * n > knight > 320
+         * b > bishop > 330
+         * r > rook > 500
+         * q > queen > 900
+         * k > king > 20000
+         */
+        var index = j * 8 + i;
+        switch (piece.type) {
+          case "p":
+            total +=
+              piece.color == "w"
+                ? pawn_value + white_pawn[index]
+                : -(pawn_value + black_pawn[index]);
+            break;
+          case "n":
+            total +=
+              piece.color == "w"
+                ? knight_value + white_knight[index]
+                : -(knight_value + black_knight[index]);
+            break;
+          case "b":
+            total +=
+              piece.color == "w"
+                ? bishop_value + white_bishop[index]
+                : -(bishop_value + black_bishop[index]);
+            break;
+          case "r":
+            total +=
+              piece.color == "w"
+                ? rook_value + white_rook[index]
+                : -(rook_value + black_rook[index]);
+            break;
+          case "q":
+            total +=
+              piece.color == "w"
+                ? queen_value + white_queen[index]
+                : -(queen_value + black_queen[index]);
+            break;
+          case "k":
+            total +=
+              piece.color == "w"
+                ? king_value + white_king[index]
+                : -(king_value + black_king[index]);
+            break; // CHANGE TO "w/b_king_endgame" table on endgame positions
+        }
+        /* Also give bonuses and maluses
+         * ADD BISHOP PAIR BONUS
+         * ADD KNIGHT PAIR MALUS
+         * ADD PAWN STRUCTURE BONUS / MALUS
+         * ADD KING PROXIMITY ON ENDGAME
+         */
       }
     }
-    return bestMove;
+
+    return game.turn() === "w" ? total : -total;
   };
 
-  const negamax = (game, depth, isMaximizing) => {
+  const minimaxRoot = (game, depth) => {
+    // checking for black - minimizing player
+    const minUtility = Infinity;
+    let bestMove = null;
+
+    const moves = game.moves();
+    for (let i = 0; i < moves.length; i++) {
+      game.move(moves[i]);
+      let score = minimax(game, depth - 1);
+      if (score < minUtility) {
+        minUtility = score;
+        bestMove = moves[i];
+      }
+      game.undo();
+      // console.log(game.pgn());
+      // console.log(rootnegamax());
+      return bestMove;
+    }
+  };
+
+  // white is maximizing player
+  const minimax = (game, depth, white) => {
+    // console.count();
     if (depth === 0) {
-      return boardScore(game.fen());
+      return evaluate();
     }
-    const possibleMoves = game.moves();
-    let bestScore = -9999;
-    for (const move of possibleMoves) {
-      game.move(move);
-      const value = -negamax(game, depth - 1, !isMaximizing);
+
+    const moves = game.moves();
+
+    if (white) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < moves.length; i++) {
+        game.move(moves[i]);
+        let score = minimax(game, depth - 1, false);
+        bestScore = Math.max(bestScore, score);
+        game.undo();
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < moves.length; i++) {
+        game.move(moves[i]);
+        let score = minimax(game, depth - 1, true);
+        bestScore = Math.min(bestScore, score);
+        game.undo();
+      }
+      return bestScore;
+    }
+  };
+
+  // Negamax algorithm root
+  var rootnegamax = function () {
+    // var start_time = performance.now()
+    var depth = 2;
+    var moves = game.moves();
+    var max = -Infinity;
+    var best_move;
+
+    for (var i in moves) {
+      game.move(moves[i]);
+      var score = -negamax(depth - 1, -999999, 999999);
       game.undo();
-      if (isMaximizing) {
-        if (value > bestScore) {
-          bestScore = value;
-        }
-      } else {
-        if (value < bestScore) {
-          bestScore = value;
-        }
+
+      if (score > max) {
+        max = score;
+        best_move = moves[i];
       }
     }
-    return bestScore;
+
+    // console.log('TIME: ' + Math.round(performance.now() - start_time) / 1000)
+    return best_move;
   };
 
-  const randomMove = () => {
-    const randomIndex = (arraylength) =>
-      Math.floor(Math.random() * arraylength);
+  var negamax = function (depth, alpha, beta) {
+    if (depth == 0) {
+      return evaluate();
+    }
+    var moves = game.moves();
+    var l = moves.length;
+    for (var i = 0; i < l; i++) {
+      game.move(moves[i]);
+      var score = -negamax(depth - 1, -beta, -alpha);
+      game.undo();
 
-    const possibleMoves = game.moves();
-    // console.log(possibleMoves);
-    // a capture will have x inside, eg. 'dxe5'
+      if (score >= beta) {
+        return score;
+      } // beta cutoff
+      if (score > alpha) {
+        alpha = score;
+      }
+    }
 
-    // const moveWithCapture = possibleMoves.filter((move) => move.includes("x"));
-    // if (moveWithCapture) {
-    //   game.move(moveWithCapture[randomIndex(moveWithCapture.length)]);
-    // }
-
-    // game.move(possibleMoves[randomIndex(possibleMoves.length)]);
-    // setPosition(game.fen());
-
-    // console.log(boardScore(game.fen()));
-    // console.log(negamax(game, 1, false));
-    console.log(rootNegamax(game, 2, false));
-
-    // timer();
+    return alpha;
   };
+
+  var black_pawn = [
+    0, 0, 0, 0, 0, 0, 0, 0, 50, 50, 50, 50, 50, 50, 50, 50, 10, 10, 20, 30, 30,
+    20, 10, 10, 5, 5, 10, 25, 25, 10, 5, 5, 0, 0, 0, 20, 20, 0, 0, 0, 5, -5,
+    -10, 0, 0, -10, -5, 5, 5, 10, 10, -20, -20, 10, 10, 5, 0, 0, 0, 0, 0, 0, 0,
+    0,
+  ];
+
+  var white_pawn = reverse_array(black_pawn);
+
+  var black_knight = [
+    -50, -40, -30, -30, -30, -30, -40, -50, -40, -20, 0, 0, 0, 0, -20, -40, -30,
+    0, 10, 15, 15, 10, 0, -30, -30, 5, 15, 20, 20, 15, 5, -30, -30, 0, 15, 20,
+    20, 15, 0, -30, -30, 5, 10, 15, 15, 10, 5, -30, -40, -20, 0, 5, 5, 0, -20,
+    -40, -50, -40, -30, -30, -30, -30, -40, -50,
+  ];
+
+  var white_knight = reverse_array(black_knight);
+
+  var black_bishop = [
+    -20, -10, -10, -10, -10, -10, -10, -20, -10, 0, 0, 0, 0, 0, 0, -10, -10, 0,
+    5, 10, 10, 5, 0, -10, -10, 5, 5, 10, 10, 5, 5, -10, -10, 0, 10, 10, 10, 10,
+    0, -10, -10, 10, 10, 10, 10, 10, 10, -10, -10, 5, 0, 0, 0, 0, 5, -10, -20,
+    -10, -10, -10, -10, -10, -10, -20,
+  ];
+
+  var white_bishop = reverse_array(black_bishop);
+
+  var black_rook = [
+    0, 0, 0, 0, 0, 0, 0, 0, 5, 10, 10, 10, 10, 10, 10, 5, -5, 0, 0, 0, 0, 0, 0,
+    -5, -5, 0, 0, 0, 0, 0, 0, -5, -5, 0, 0, 0, 0, 0, 0, -5, -5, 0, 0, 0, 0, 0,
+    0, -5, -5, 0, 0, 0, 0, 0, 0, -5, 0, 0, 0, 5, 5, 0, 0, 0,
+  ];
+
+  var white_rook = reverse_array(black_rook);
+
+  var black_queen = [
+    -20, -10, -10, -5, -5, -10, -10, -20, -10, 0, 0, 0, 0, 0, 0, -10, -10, 0, 5,
+    5, 5, 5, 0, -10, -5, 0, 5, 5, 5, 5, 0, -5, 0, 0, 5, 5, 5, 5, 0, -5, -10, 5,
+    5, 5, 5, 5, 0, -10, -10, 0, 5, 0, 0, 0, 0, -10, -20, -10, -10, -5, -5, -10,
+    -10, -20,
+  ];
+
+  var white_queen = reverse_array(black_queen);
+
+  var black_king = [
+    -30, -40, -40, -50, -50, -40, -40, -30, -30, -40, -40, -50, -50, -40, -40,
+    -30, -30, -40, -40, -50, -50, -40, -40, -30, -30, -40, -40, -50, -50, -40,
+    -40, -30, -20, -30, -30, -40, -40, -30, -30, -20, -10, -20, -20, -20, -20,
+    -20, -20, -10, 20, 20, 0, 0, 0, 0, 20, 20, 20, 30, 10, 0, 0, 10, 30, 20,
+  ];
+
+  var white_king = reverse_array(black_king);
+
+  var black_king_endgame = [
+    -50, -40, -30, -20, -20, -30, -40, -50, -30, -20, -10, 0, 0, -10, -20, -30,
+    -30, -10, 20, 30, 30, 20, -10, -30, -30, -10, 30, 40, 40, 30, -10, -30, -30,
+    -10, 30, 40, 40, 30, -10, -30, -30, -10, 20, 30, 30, 20, -10, -30, -30, -30,
+    0, 0, 0, 0, -30, -30, -50, -30, -30, -30, -30, -30, -30, -50,
+  ];
+
+  var white_king_endgame = reverse_array(black_king_endgame);
 
   return (
     <div className={styles.container}>
@@ -142,6 +350,7 @@ export default function Home() {
           transitionDuration={100}
           onDrop={onDrop}
         />
+        <h2>Gamestate: {gameState}</h2>
       </main>
 
       <footer className={styles.footer}>
