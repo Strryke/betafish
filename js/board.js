b4f154e6836983d48e01a512029efda8fe1e197c
Strryke <gavinong2000@gmail.com>
2022-09-17 18:59:15 +0800

    ok fresh start time

--- js/board.js
+++ js/board.js
@@ -1,123 +1,164 @@
-var whiteSquareGrey = "#a9a9a9";
-var blackSquareGrey = "#696969";
 
-function removeGreySquares() {
-  $("#myBoard .square-55d63").css("background", "");
+function PCEINDEX(pce, pceNum) {
+	return (pce * 10 + pceNum);
 }
 
-function greySquare(square) {
-  var $square = $("#myBoard .square-" + square);
+var GameBoard = {};
 
-  var background = whiteSquareGrey;
-  if ($square.hasClass("black-3c85d")) {
-    background = blackSquareGrey;
-  }
+GameBoard.pieces = new Array(BRD_SQ_NUM);
+GameBoard.side = COLOURS.WHITE;
+GameBoard.fiftyMove = 0;
+GameBoard.hisPly = 0;
+GameBoard.ply = 0;
+GameBoard.enPas = 0;
+GameBoard.castlePerm = 0;
+GameBoard.material = new Array(2); // WHITE,BLACK material of pieces
+GameBoard.pceNum = new Array(13); // indexed by Pce
+GameBoard.pList = new Array(14 * 10);
+GameBoard.posKey = 0;
 
-  $square.css("background", background);
+GameBoard.moveList = new Array(MAXDEPTH * MAXPOSITIONMOVES);
+GameBoard.moveScores = new Array(MAXDEPTH * MAXPOSITIONMOVES);
+GameBoard.moveListStart = new Array(MAXDEPTH);
+
+function GeneratePosKey() {
+
+	var sq = 0;
+	var finalKey = 0;
+	var piece = PIECES.EMPTY;
+
+	for(sq = 0; sq < BRD_SQ_NUM; ++sq) {
+		piece = GameBoard.pieces[sq];
+		if(piece != PIECES.EMPTY && piece != SQUARES.OFFBOARD) {			
+			finalKey ^= PieceKeys[(piece * 120) + sq];
+		}		
+	}
+
+	if(GameBoard.side == COLOURS.WHITE) {
+		finalKey ^= SideKey;
+	}
+	
+	if(GameBoard.enPas != SQUARES.NO_SQ) {		
+		finalKey ^= PieceKeys[GameBoard.enPas];
+	}
+	
+	finalKey ^= CastleKeys[GameBoard.castlePerm];
+	
+	return finalKey;
+
 }
 
-function onDragStart(source, piece) {
-  // do not pick up pieces if the game is over
-  if (game.game_over()) return false;
-
-  // or if it's not that side's turn
-  if (
-    (game.turn() === "w" && piece.search(/^b/) !== -1) ||
-    (game.turn() === "b" && piece.search(/^w/) !== -1)
-  ) {
-    return false;
-  }
+function ResetBoard() {
+	
+	var index = 0;
+	
+	for(index = 0; index < BRD_SQ_NUM; ++index) {
+		GameBoard.pieces[index] = SQUARES.OFFBOARD;
+	}
+	
+	for(index = 0; index < 64; ++index) {
+		GameBoard.pieces[SQ120(index)] = PIECES.EMPTY;
+	}
+	
+	for(index = 0; index < 14 * 120; ++index) {
+		GameBoard.pList[index] = PIECES.EMPTY;
+	}
+	
+	for(index = 0; index < 2; ++index) {		
+		GameBoard.material[index] = 0;		
+	}	
+	
+	for(index = 0; index < 13; ++index) {
+		GameBoard.pceNum[index] = 0;
+	}
+	
+	GameBoard.side = COLOURS.BOTH;
+	GameBoard.enPas = SQUARES.NO_SQ;
+	GameBoard.fiftyMove = 0;	
+	GameBoard.ply = 0;
+	GameBoard.hisPly = 0;	
+	GameBoard.castlePerm = 0;	
+	GameBoard.posKey = 0;
+	GameBoard.moveListStart[GameBoard.ply] = 0;
+	
 }
 
-function onMouseoverSquare(square, piece) {
-  // get list of possible moves for this square
-  var moves = game.moves({
-    square: square,
-    verbose: true,
-  });
+function ParseFen(fen) {
 
-  // exit if there are no moves available for this square
-  if (moves.length === 0) return;
+	ResetBoard();
 
-  // highlight the square they moused over
-  greySquare(square);
-
-  // highlight the possible squares for this piece
-  for (var i = 0; i < moves.length; i++) {
-    greySquare(moves[i].to);
-  }
 }
 
-function onMouseoutSquare(square, piece) {
-  removeGreySquares();
-}
 
-function onSnapEnd() {
-  board.position(game.fen());
-}
-const onDrop = (sourceSquare, targetSquare) => {
-  removeGreySquares();
-  // see if the move is legal
-  var move = game.move({
-    from: sourceSquare,
-    to: targetSquare,
-    promotion: "q", // NOTE: always promote to a queen for example simplicity
-  });
 
-  // illegal move
-  if (move === null) return "snapback";
-  setTimeout(() => makeMove(), 200);
-};
 
-function updateStatus() {
-  var status = "";
 
-  var moveColor = "White";
-  if (game.turn() === "b") {
-    moveColor = "Black";
-  }
 
-  // checkmate?
-  if (game.in_checkmate()) {
-    status = "Game over, " + moveColor + " is in checkmate.";
-  }
 
-  // draw?
-  else if (game.in_draw()) {
-    status = "Game over, drawn position";
-  }
 
-  // game still on
-  else {
-    status = moveColor + " to move";
 
-    // check?
-    if (game.in_check()) {
-      status += ", " + moveColor + " is in check";
-    }
-  }
 
-  $status.html(status);
-  $fen.html(game.fen());
-  $pgn.html(game.pgn());
-  $nodes.html(nodes);
-  $quiescence_nodes.html(quiescence_nodes);
-  $time_taken.html((time_taken / 1000).toFixed(2) + "s");
-  $evaluated_score.html(evaluate());
 
-  quiescence_nodes = 0;
-  nodes = 0;
-  time_taken = 0;
-}
 
-var config = {
-  draggable: true,
-  position: "start",
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onMouseoutSquare: onMouseoutSquare,
-  onMouseoverSquare: onMouseoverSquare,
-  onSnapEnd: onSnapEnd,
-};
 
-board = Chessboard("myBoard", config);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,163 +1,123 @@
-var board = null;
-var game = new Chess();
-
-var $status = $("#status");
-var $fen = $("#fen");
-var $pgn = $("#pgn");
-var $nodes = $("#nodes");
-var $quiescence_nodes = $("#quiescenceNodes");
-var $time_taken = $("#timeTaken");
-var $getMove = $("#getMove");
-var $setFen = $("#setFEN");
-var $evaluated_score = $("#evaluatedScore");
-
-var time_taken = 0;
-var nodes = 0;
-var quiescence_nodes = 0;
-
-$getMove.click(() => {
-  // console.log(getBestMove());
-  // console.log(game.ugly_moves());
-  // game.ugly_move(getBestMove());
-  game.ugly_move(getBestMove());
-  board.position(game.fen());
-  updateStatus();
-  console.log(endgame);
+$(function() {
+	init();
+	console.log("Main Init Called");	
 });
 
-$setFen.click(() => {
-  let newFen = $("#fen").val();
-  console.log(newFen);
-  // game = new Chess(newFen);
-  game.load(newFen);
-  board.position(game.fen());
-});
+function InitFilesRanksBrd() {
+	
+	var index = 0;
+	var file = FILES.FILE_A;
+	var rank = RANKS.RANK_1;
+	var sq = SQUARES.A1;
+	
+	for(index = 0; index < BRD_SQ_NUM; ++index) {
+		FilesBrd[index] = SQUARES.OFFBOARD;
+		RanksBrd[index] = SQUARES.OFFBOARD;
+	}
+	
+	for(rank = RANKS.RANK_1; rank <= RANKS.RANK_8; ++rank) {
+		for(file = FILES.FILE_A; file <= FILES.FILE_H; ++file) {
+			sq = FR2SQ(file,rank);
+			FilesBrd[sq] = file;
+			RanksBrd[sq] = rank;
+		}
+	}
+	
+	console.log("FilesBrd[0]:" + FilesBrd[0] + " RanksBrd[0]:" + RanksBrd[0]);
+	console.log("FilesBrd[SQUARES.A1]:" + FilesBrd[SQUARES.A1] + " RanksBrd[SQUARES.A1]:" + RanksBrd[SQUARES.A1]);
+	console.log("FilesBrd[SQUARES.E8]:" + FilesBrd[SQUARES.E8] + " RanksBrd[SQUARES.E8]:" + RanksBrd[SQUARES.E8]);
 
-function makeMove() {
-  game.ugly_move(getBestMove());
-  board.position(game.fen());
-  updateStatus();
 }
 
-// AI Game Logic
-
-function quiescence(alpha, beta, depth) {
-  quiescence_nodes++;
-  const evaluation = evaluate();
-
-  if (depth <= 0) return alpha;
-  if (evaluation >= beta) return beta;
-  if (alpha < evaluation) alpha = evaluation;
-
-  var specialMoves = [];
-  const uglyMoves = game.ugly_moves();
-
-  for (let i = 0; i < uglyMoves.length; i++) {
-    game.ugly_move(uglyMoves[i]);
-    if (game.in_check() || game.in_checkmate()) {
-      specialMoves.push(uglyMoves[i]);
-    }
-    game.undo();
-
-    // moves with takes
-    if (uglyMoves[i].captured != undefined) {
-      specialMoves.push(uglyMoves[i]);
-    }
-  }
-
-  for (var i = 0; i < specialMoves.length; i++) {
-    game.ugly_move(specialMoves[i]);
-    var score = -quiescence(-beta, -alpha, depth - 1);
-    game.undo();
-    if (score >= beta) return beta;
-    if (score > alpha) alpha = score;
-  }
-
-  return alpha;
+function InitHashKeys() {
+    var index = 0;
+	
+	for(index = 0; index < 14 * 120; ++index) {				
+		PieceKeys[index] = RAND_32();
+	}
+	
+	SideKey = RAND_32();
+	
+	for(index = 0; index < 16; ++index) {
+		CastleKeys[index] = RAND_32();
+	}
 }
 
-getOrderedMoves();
+function InitSq120To64() {
 
-function evaluateMoveScore(move) {
-  // promotions -> captures -> normal moves
-  const captureScores = {
-    q: 9,
-    r: 5,
-    b: 3,
-    n: 3,
-    p: 1,
-  };
+	var index = 0;
+	var file = FILES.FILE_A;
+	var rank = RANKS.RANK_1;
+	var sq = SQUARES.A1;
+	var sq64 = 0;
 
-  if (move.promotion != undefined) return { ...move, score: 10 };
+	for(index = 0; index < BRD_SQ_NUM; ++index) {
+		Sq120ToSq64[index] = 65;
+	}
+	
+	for(index = 0; index < 64; ++index) {
+		Sq64ToSq120[index] = 120;
+	}
+	
+	for(rank = RANKS.RANK_1; rank <= RANKS.RANK_8; ++rank) {
+		for(file = FILES.FILE_A; file <= FILES.FILE_H; ++file) {
+			sq = FR2SQ(file,rank);
+			Sq64ToSq120[sq64] = sq;
+			Sq120ToSq64[sq] = sq64;
+			sq64++;
+		}
+	}
 
-  if (move.captured != undefined)
-    return {
-      ...move,
-      score: captureScores[move.captured] - captureScores[move.piece],
-    };
-
-  return { ...move, score: 0 };
 }
 
-function getOrderedMoves() {
-  const moves = game.ugly_moves();
-  const movesWithScores = moves.map((move) => evaluateMoveScore(move));
-  const sortedMoves = movesWithScores.sort((a, b) => b.score - a.score);
-
-  return sortedMoves;
+function init() {
+	console.log("init() called");
+	InitFilesRanksBrd();
+	InitHashKeys();
+	InitSq120To64();
 }
 
-function getBestMove() {
-  var start_time = performance.now();
-  var depth = endgame ? 4 : 3;
-  // var moves = game.moves();
-  // const moves = getOrderedMoves();
-  // const moves = game.ugly_moves();
-  const moves = getOrderedMoves();
-  var max = -Infinity;
-  var best_move;
 
-  for (var i in moves) {
-    game.ugly_move(moves[i]);
-    var score = -negamax(depth - 1, -999999, 999999);
-    game.undo();
 
-    if (score > max) {
-      max = score;
-      best_move = moves[i];
-    }
-  }
 
-  var end_time = performance.now();
-  time_taken = end_time - start_time;
 
-  return best_move;
-}
 
-function negamax(depth, alpha, beta) {
-  nodes++;
-  if (depth == 0) {
-    // return evaluate();
-    return quiescence(alpha, beta, 4);
-  }
 
-  const moves = getOrderedMoves();
-  var l = moves.length;
-  for (var i = 0; i < l; i++) {
-    game.ugly_move(moves[i]);
-    var score = -negamax(1 - 1, -beta, -alpha);
-    game.undo();
 
-    if (score >= beta) {
-      return beta;
-    } // beta cutoff
-    if (score > alpha) {
-      alpha = score;
-    }
-  }
 
-  if (game.in_draw() || game.in_stalemate() || game.in_threefold_repetition())
-    return 0;
 
-  return alpha;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
