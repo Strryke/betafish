20128699687ac156c4112ed0f4a5bf64679016d0
Strryke <gavinong2000@gmail.com>
2022-09-17 22:12:00 +0800

    movegen[0]

--- js/defs.js
+++ js/defs.js
@@ -268,3 +268,40 @@ var PceChar = ".PNBRQKpnbrqk";
 var SideChar = "wb-";
 var RankChar = "12345678";
 var FileChar = "abcdefgh";
+
+/*	
+0000 0000 0000 0000 0000 0111 1111 -> From 0x7F
+0000 0000 0000 0011 1111 1000 0000 -> To >> 7, 0x7F
+0000 0000 0011 1100 0000 0000 0000 -> Captured >> 14, 0xF
+0000 0000 0100 0000 0000 0000 0000 -> EP 0x40000
+0000 0000 1000 0000 0000 0000 0000 -> Pawn Start 0x80000
+0000 1111 0000 0000 0000 0000 0000 -> Promoted Piece >> 20, 0xF
+0001 0000 0000 0000 0000 0000 0000 -> Castle 0x1000000
+*/
+
+function FROMSQ(m) {
+  return m & 0x7f;
+}
+function TOSQ(m) {
+  return (m >> 7) & 0x7f;
+}
+function CAPTURED(m) {
+  return (m >> 14) & 0xf;
+}
+function PROMOTED(m) {
+  return (m >> 20) & 0xf;
+}
+
+var MFLAGEP = 0x40000;
+var MFLAGPS = 0x80000;
+var MFLAGCA = 0x1000000;
+
+var MFLAGCAP = 0x7c000;
+var MFLAGPROM = 0xf00000;
+
+var NOMOVE = 0;
+
+function SQOFFBOARD(sq) {
+  if (FilesBrd[sq] == SQUARES.OFFBOARD) return BOOL.TRUE;
+  return BOOL.FALSE;
+}
